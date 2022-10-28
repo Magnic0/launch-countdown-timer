c1b914cffb28eaf44ce13d348a9125fdedd53a61
Nicolas Oliveira <nicolsoliverg@gmail.com>
2022-10-28 17:34:01 -0300

    rename and move main jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './app/App';
+import { Header } from './app/Header';
+import { Timer } from './app/Timer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Timer />
   </React.StrictMode>
 )
