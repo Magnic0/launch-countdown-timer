a82b3dd4dbc0cec7e5db4e0b0f5bc8543cdf5e57
Magnic0 <nicolsoliverg@gmail.com>
2022-11-20 18:45:52 -0300

    React: update and optimize Timer component and its child component

--- src/app/Timer/Timer.jsx
+++ src/app/Timer/Timer.jsx
@@ -38,7 +38,8 @@ export function parseTwoDig(num) {
 
 // main component
 export function Timer() {
-  const [state, setState] = useState(timeInSeconds);
+  const [state, setState] = useState(timeInSeconds); // current state
+  const [pState, setPState] = useState(timeInSeconds - 1); // previous state
 
   // trigger an alert when time reach zero
   if (state === 0) {
@@ -47,53 +48,66 @@ export function Timer() {
     }, 200);
   }
   
-  // make time pass
+  // make time pass for both states
   if (state > 0) {
     setTimeout(() => {
       setState(state - 1);
+      setPState(pState - 1);
     }, 1000);
   }
 
   // specify seconds state to fulfill each timestamp
+  //current
   const minState = Math.floor(state / 60);
   const hourState = Math.floor(minState / 60);
   const dayState = Math.floor(hourState / 24);
+  //previous
+  const pMinState = Math.floor(pState / 60);
+  const pHourState = Math.floor(pMinState / 60);
+  const pDayState = Math.floor(pHourState / 24);
 
-  // transform actual state to show each timestamp in app frontend
+  // transform state to show each timestamp in app frontend
+  // current
   const seconds = state % 60;
   const minutes = minState % 60;
   const hours = hourState % 24;
   const days = dayState;
+  //previous
+  const pSeconds = pState % 60;
+  const pMinutes = pMinState % 60;
+  const pHours = pHourState % 24;
+  const pDays = pDayState;
 
   return (
     <section className='timer'>
-      <TimerNumber
-        timeState={days}
-        unfoldClass={unfoldClass}
-        foldClass={foldClass}
-        timeStamp={"days"}
-      />
-
-      <TimerNumber
-        timeState={hours}
-        unfoldClass={unfoldClass}
-        foldClass={foldClass}
-        timeStamp={"hours"}
-      />
-
-      <TimerNumber
-        timeState={minutes}
-        unfoldClass={unfoldClass}
-        foldClass={foldClass}
-        timeStamp={"minutes"}
-      />
-
-      <TimerNumber
-        timeState={seconds}
-        unfoldClass={unfoldClass}
-        foldClass={foldClass}
-        timeStamp={"seconds"}
-      />
+        <TimerNumber
+          timeState={days}
+          pTimeState={pDays}
+          unfoldClass={unfoldClass}
+          foldClass={foldClass}
+          timeStamp={"days"}
+        />
+        <TimerNumber
+          timeState={hours}
+          pTimeState={pHours}
+          unfoldClass={unfoldClass}
+          foldClass={foldClass}
+          timeStamp={"hours"}
+        />
+        <TimerNumber
+          timeState={minutes}
+          pTimeState={pMinutes}
+          unfoldClass={unfoldClass}
+          foldClass={foldClass}
+          timeStamp={"minutes"}
+        />
+        <TimerNumber
+          timeState={seconds}
+          pTimeState={pSeconds}
+          unfoldClass={unfoldClass}
+          foldClass={foldClass}
+          timeStamp={"seconds"}
+        />
     </section>
   )
 }
--- src/app/Timer/TimerNumber.jsx
+++ src/app/Timer/TimerNumber.jsx
@@ -1,38 +1,38 @@
 import { useState, useEffect } from "react";
 import { parseTwoDig } from "./Timer";
 
-export function TimerNumber({ timeState, unfoldClass, foldClass, timeStamp }) {
-     const [flipSec, setFlipSec] = useState(false);
-     useEffect(() => {
-       setFlipSec(true);
-       setTimeout(() => {
-         setFlipSec(false);
-       }, 900);
-     }, [timeState]);
-     
-     return (
-          <div className='timer__in'>
-               <div className='timer__front'>
-                    <div className={'timer__front--top'}>
-                         <p>{parseTwoDig(timeState)}</p>
-                    </div>
+export function TimerNumber({ timeState, pTimeState, foldClass, unfoldClass, timeStamp }) {
+    const [flipSec, setFlipSec] = useState(false);
 
-                    <div className={'timer__front--bot' + (flipSec ? unfoldClass : '')}>
-                         <p>{parseTwoDig(timeState)}</p>
-                    </div>
-               </div>
+    useEffect(() => {
+        setFlipSec(false);
+        
+        setTimeout(() => {
+            setFlipSec(true);
+        }, 100);
+    }, [pTimeState]);
 
-               <div className='timer__old'>
-                    <div className={'timer__old--top' + (flipSec ? foldClass : '')}>
-                         <p>{parseTwoDig(timeState)}</p>
-                    </div>
+    return (
+        <div className='timer__in'>
+            <div className='timer__front'>
+                <div className={'timer__front--top'}>
+                    <p>{parseTwoDig(pTimeState)}</p>
+                </div>
 
-                    <div className={'timer__old--bot'}>
-                         <p>{parseTwoDig(timeState)}</p>
-                    </div>
-               </div>
+                <div className={'timer__front--bot' + (flipSec ? unfoldClass : '')}>
+                    <p>{parseTwoDig(pTimeState)}</p>
+                </div>
+            </div>
+            <div className='timer__old'>
+                <div className={'timer__old--top' + (flipSec ? foldClass : '')}>
+                    <p>{parseTwoDig(timeState)}</p>
+                </div>
 
-               <p>{timeStamp}</p>
-          </div>
-     );
+                <div className={'timer__old--bot' + (flipSec ? ' fade' : '')}>
+                    <p>{parseTwoDig(timeState)}</p>
+                </div>
+            </div>
+            <p>{timeStamp}</p>
+        </div>
+    )
 }
